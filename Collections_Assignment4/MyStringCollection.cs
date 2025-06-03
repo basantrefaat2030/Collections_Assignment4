7107c80cbd9a59a33669080616c01a4015be25b9
refaatbasant2030 <refaatbasant2030@gmail.com>
2025-06-03 05:46:46 +0300

    implement MyStringCollectionClass

--- Collections_Assignment4/MyStringCollection.cs
+++ Collections_Assignment4/MyStringCollection.cs
@@ -8,6 +8,61 @@ namespace Collections_Assignment4
 {
     public class MyStringCollection
     {
-        public List<string>
+        List<string> stringColl ;
+
+        public MyStringCollection()
+        {
+            stringColl = new List<string>();
+        
+        }
+
+        void Add(string item)
+        {
+            if (item != null)
+            {
+                if (stringColl.Contains(item))
+                {
+                    Remove(item);
+                    stringColl.Add(item);
+                }
+                stringColl.Add(item.ToString());
+            }
+        }
+
+        void Remove(string item)
+        {
+            if(item != null) 
+            { 
+                stringColl.Remove(item); 
+            }
+        }
+
+        bool Contains(string item)
+        {
+            if(item != null)
+            {
+                if (stringColl.Contains(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        void PrintAll()
+        {
+            if(stringColl is not null)
+            {
+                foreach(string item in stringColl)
+                {
+                    Console.WriteLine($"\n {item}");
+                }
+            }
+
+            Console.WriteLine("");
+        }
+
     }
 }
